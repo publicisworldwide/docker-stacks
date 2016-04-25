f896f4c748135f71d59226f905dd1de602ecd1a0
Sebastian Küpers <sebastian.kuepers@publicispixelpark.de>
2016-04-25 12:17:59 +0200

    moved port change from Dockerfile to entrypoint.sh

--- oracle-linux/environments/storage/redis-cluster/entrypoint.sh
+++ oracle-linux/environments/storage/redis-cluster/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+sed -i 's/REDIS_PORT/'$REDIS_PORT'/g' /usr/local/etc/redis.conf
+
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
 	chown -R redis .
