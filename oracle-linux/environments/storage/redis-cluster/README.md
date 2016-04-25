20d855a5e768f095f376911f0329b25996bc81cd
Hannes Eichblatt <hannes.eichblatt@publicispixelpark.de>
2016-04-25 16:34:25 +0200

    redis-cluster - Bugfix entrypoint

---
+++ oracle-linux/environments/storage/redis-cluster/README.md
@@ -0,0 +1,5 @@
+# Redis Cluster
+
+You can change the Redis port by setting REDIS_PORT when running the container.
+
+    docker run -e REDIS_PORT=9999 publicisworldwide/redis-cluster
--- oracle-linux/environments/storage/redis-cluster/entrypoint.sh
+++ oracle-linux/environments/storage/redis-cluster/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-set -e
 
-sed -i 's/REDIS_PORT/'$REDIS_PORT'/g' /usr/local/etc/redis.conf
+set -e
 
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
+	sed -i 's/REDIS_PORT/'$REDIS_PORT'/g' /usr/local/etc/redis.conf
 	chown -R redis .
 	exec gosu redis "$0" "$@"
 fi
