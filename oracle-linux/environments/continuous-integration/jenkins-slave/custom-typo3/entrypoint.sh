1934cbc3254102c686c0faee790dd5e0c564ebf8
Hannes Eichblatt <hannes.eichblatt@publicispixelpark.de>
2016-11-22 13:55:32 +0100

    jenkins-slave:custom-typo3 - shorter entrypoint

--- oracle-linux/environments/continuous-integration/jenkins-slave/custom-typo3/entrypoint.sh
+++ oracle-linux/environments/continuous-integration/jenkins-slave/custom-typo3/entrypoint.sh
@@ -14,17 +14,3 @@ for i in $SERVICES ; do
   /etc/init.d/"$i" start
   /etc/init.d/"$i" status || true   # do not error if init script has no "status"
 done
-
-echo Waiting 2s..
-sleep 2s
-
-echo Starting tails..
-
-for logfile in /var/log/phantomjs/main.log /var/log/httpd/{error,access}_log /var/log/selenium-server/*log ; do
-  echo Executing tail -f "$logfile" ..
-  tail -f "$logfile" &
-done
-
-echo Startup done. Entering infinite loop..
-
-while : ; do sleep 1s ; done
